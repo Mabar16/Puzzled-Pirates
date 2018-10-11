5e6503b2cc111fbf045a2cd1f863b109435085c4
Markus <mabar16@student.sdu.dk>
2018-10-11 10:45:51 +0200

    Initial Commit

---
+++ Puzzled Pirates/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Puzzled Pirates
+//
+//  Created by Markus Barrow on 11/10/2018.
+//  Copyright © 2018 University of Southern Denmark. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
