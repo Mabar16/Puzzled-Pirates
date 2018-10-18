1d5c2b0684103cf8b8cfb2e2f4347916b058ee65
mforberg <mifor16@student.sdu.dk>
2018-10-18 12:25:16 +0200

    Map Basic Implementation

---
+++ Puzzled Pirates/MapKitViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MapKitViewController.swift
+//  Puzzled Pirates
+//
+//  Created by Mikkel on 18/10/2018.
+//  Copyright © 2018 University of Southern Denmark. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapKitViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocationServices()
+        print("OHIO GOZAIMASUUUUU")
+        
+        
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // no allowed to locate
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        print(CLLocationManager.authorizationStatus().rawValue)
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .authorizedAlways:
+            break
+        case .denied:
+            // show instruction how fix
+            break
+        case .restricted:
+            // show that there's parental controls enabled
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+            
+        }
+    }
+
+    
+}
+
+extension MapKitViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+}
