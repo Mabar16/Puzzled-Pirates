a371412913d1f5266398de0a1514cfea17d83435
Mikkel <m.forberg@hotmail.com>
2018-10-25 08:46:24 +0200

    pls werk

--- Puzzled Pirates/SecondViewController.swift
+++ Puzzled Pirates/SecondViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var puzzleLabel: UILabel!
+    @IBOutlet weak var puzzleImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        puzzleLabel?.text = labelText()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func showPuzzle() {
+        puzzleLabel?.isHidden = !checkLocation()
+    }
+    
+    func labelText() -> String {
+        if checkLocation() {
+            return "X's and O's"
+        } else {
+            return "Please find the right location"
+        }
+    }
+    
+    func checkLocation() -> Bool {
+        return false
+    }
 }
 
