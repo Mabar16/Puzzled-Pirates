78139a9ff4a93eedeaf1d441fe2620fca93a4350
Markus <mabar16@student.sdu.dk>
2018-11-08 09:43:21 +0100

    OutSiderGameArray
    bobby

--- OutsideLogic.swift
+++ OutsideLogic.swift
@@ -10,17 +10,29 @@ import Foundation
 
 class OutsiderLogic
 {
-
     
     public func gameTitle() -> String {
         return "Which one does not belong?";
     }
     
-    func makeRounds2() -> [OutsiderRound]{
-        var outsiderRound = OutsiderRound.init(outsider: "Plane", matchingWords: ["Cat", "Dog", "Bird"]);
-        var outsiderRoundArray = [outsiderRound]
+    var roundArray: [OutsiderRound]? = nil;
+    var numberOfRounds: Int = 0;
+    var roundCounter: Int = 0;
+    
+    init() {
+        let tuple = self.makeRounds2()
+        roundArray = tuple.0;
+        numberOfRounds = tuple.1;
+    }
+    
+    func makeRounds2() -> ([OutsiderRound], Int) {
+        let outsiderRound = OutsiderRound.init(outsider: "Plane", matchingWords: ["Cat", "Dog", "Bird"]);
+        let outsiderRound2 = OutsiderRound.init(outsider: "Brown", matchingWords: ["Blue", "Yellow", "Red"]);
+        let outsiderRound3 = OutsiderRound.init(outsider: "Human", matchingWords: ["Mosquito", "Dragonfly", "Fly"]);
+        let outsiderRound4 = OutsiderRound.init(outsider: "Cola", matchingWords: ["Ham", "Cheese", "Egg"]);
+        let outsiderRoundArray = [outsiderRound, outsiderRound2, outsiderRound3, outsiderRound4]
         
-        return outsiderRoundArray
+        return (outsiderRoundArray, outsiderRoundArray.count)
     }
     
     
